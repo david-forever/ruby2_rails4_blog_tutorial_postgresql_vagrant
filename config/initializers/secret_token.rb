82cea6808958c9cbf2f73067397c44c128986900
David Watson <dwatson@forever.com>
2013-07-01 18:39:05 -0400

    Initial checkin

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+BlogPg::Application.config.secret_token = 'eb6611efb6518ff721b5a7c196f5f9563d34602991661002a6a27fe53117b82220eef6e8bcd85151af99cb16af49015248efb6e5fbd5f2267b74d046ad164084'
